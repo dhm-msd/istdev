6f471f344cb29f31a69236edaf1192ccdb42eb67
ISTDev <dhm.msd@icloud.com>
2017-05-14 12:12:06 +0300

    +Temporary Demo functions +heartbeat functionality that allows the server to communicate automatically with the clients every 15 mins

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var priority_request = require('./routes/priority_request');
 var measurements = require('./routes/measurements');
-
+//var demo_request = require('./routes/demo')
 var app = express();
 //Database Connector
 var database = require('./database_manager.js')
@@ -27,28 +27,59 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/priority', priority_request)
 app.use('/measurements', measurements)
 
+
+//These routes will be used to demo the server-client communication and control
+app.use('/demo_on', demo_request_on)
+app.use('/demo_off', demo_request_off)
+
+function demo_request_on(req, res) {
+    res.send("Demo ON request. Connected clients: "+connected_clients.length)
+    connected_clients.forEach(function(entry) {
+        console.log(entry);
+        entry.write("switch_on\n")
+    });
+}
+function demo_request_off(req, res) {
+    res.send("Demo OFF begin. Connected clients: "+connected_clients.length)
+    connected_clients.forEach(function(entry) {
+        console.log(entry);
+        entry.write("switch_off\n")
+    });
+}
+//-----------------------------------------
+//This method will be responsible for the server-client auto communication
+//ex: Measurements,Priority change, alerts push
+function heartbeat(){
+    console.log('heartbeat')
+    if(connected_clients.length > 0){
+        connected_clients.forEach(function(entry) {
+            entry.write("measurements\n")
+        });
+    }
+}
+setInterval(heartbeat, 900000) //Heartbeat is set for 15mins
+
 //Handle for the keep-alive connection with the clients.
 //The clients will connect to this hook and will listen for messages/commands to execute
 app.get('/update', updateMiddleware);
-
+var connected_clients = [];
 function updateMiddleware(req, res) {
+    connected_clients.push(res)
     res.setHeader('Connection', 'keep-alive');
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.writeHead(200);
     setTimeout(function() {
-        res.write("this is an event\n");
-        res.flushHeaders();
-    }, 1000);
-    setTimeout(function() {
         res.write("measurements\n");
     }, 9000);
     setTimeout(function() {
-        res.write("multi thread magics\n");
-    }, 9000);
+        res.write("switch_on\n");
+    }, 10000);
     setTimeout(function() {
-        res.write("measurements\n");
-    }, 14000);
+        res.write("switch_off\n");
+    }, 15000);
+    console.log(connected_clients)
+
 }
 
 //Handles POST requests to measurements endpoint.
